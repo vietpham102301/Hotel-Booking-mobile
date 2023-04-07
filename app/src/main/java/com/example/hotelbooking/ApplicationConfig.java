4785746fb68198527fa51f7c1db2d0fc286bedce
vietpham2310 <vietpham102301@gmail.com>
2023-04-07 16:12:01 +0700

    init

---
+++ app/src/main/java/com/example/hotelbooking/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.example.hotelbooking;
+
+
+import android.app.Application;
+
+public class ApplicationConfig extends Application {
+    public static String flow;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
